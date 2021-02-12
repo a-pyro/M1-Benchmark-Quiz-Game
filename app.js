0c83a0f3b06c03137a07badfd7a13f061a979525
ardigermenji <ardi.germenji@gmail.com>
2021-02-12 18:07:10 +0100

    removes question array

--- app.js
+++ app.js
@@ -1,99 +1,4 @@
 console.log('Hi there! 🔥');
-const questions = [
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question: 'What does CPU stand for?',
-    correct_answer: 'Central Processing Unit',
-    incorrect_answers: [
-      'Central Process Unit',
-      'Computer Personal Unit',
-      'Central Processor Unit',
-    ],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?',
-    correct_answer: 'Final',
-    incorrect_answers: ['Static', 'Private', 'Public'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'boolean',
-    difficulty: 'easy',
-    question: 'The logo for Snapchat is a Bell.',
-    correct_answer: 'False',
-    incorrect_answers: ['True'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'boolean',
-    difficulty: 'easy',
-    question:
-      'Pointers were not used in the original C programming language; they were added later on in C++.',
-    correct_answer: 'False',
-    incorrect_answers: ['True'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'What is the most preferred image format used for logos in the Wikimedia database?',
-    correct_answer: '.svg',
-    incorrect_answers: ['.png', '.jpeg', '.gif'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question: 'In web design, what does CSS stand for?',
-    correct_answer: 'Cascading Style Sheet',
-    incorrect_answers: [
-      'Counter Strike: Source',
-      'Corrective Style Sheet',
-      'Computer Style Sheet',
-    ],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'What is the code name for the mobile operating system Android 7.0?',
-    correct_answer: 'Nougat',
-    incorrect_answers: ['Ice Cream Sandwich', 'Jelly Bean', 'Marshmallow'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question: 'On Twitter, what is the character limit for a Tweet?',
-    correct_answer: '140',
-    incorrect_answers: ['120', '160', '100'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'boolean',
-    difficulty: 'easy',
-    question: 'Linux was first created as an alternative to Windows XP.',
-    correct_answer: 'False',
-    incorrect_answers: ['True'],
-  },
-  {
-    category: 'Science: Computers',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'Which programming language shares its name with an island in Indonesia?',
-    correct_answer: 'Java',
-    incorrect_answers: ['Python', 'C', 'Jakarta'],
-  },
-];
 
 window.onload = function () {
   //memory
@@ -102,8 +7,6 @@ window.onload = function () {
   let questionNumber = 0;
   const userAnswers = [];
   let pointsMemory = 0;
-
-  //fetching questions
 
   //referenze
 
@@ -141,6 +44,7 @@ window.onload = function () {
     // console.log(quantity, difficulty);
   }
 
+  //fetch function
   async function getQuestions(quantity, difficulty) {
     const endpoint = `https://opentdb.com/api.php?amount=${quantity}&category=18&difficulty=${difficulty}`;
     const response = await fetch(endpoint);
@@ -290,11 +194,12 @@ window.onload = function () {
     console.log('userAnswers.length:', userAnswers.length);
     console.log('questionNumber:', questionNumber);
 
-    if (userAnswers.length < questionNumber) {
-      alert('must answer before!');
+    const answers = document.querySelectorAll('.answer-controller input');
+    const userInput = [...answers].find((radioBtn) => radioBtn.checked);
+    if (!userInput) {
+      alert('devi prima rispondereeeeee 💩');
       return;
     }
-
     renderQuestion(questionNumber);
   }
 
@@ -308,6 +213,9 @@ window.onload = function () {
   }
   function showFinalResult() {
     console.log('finito, ora facciamo altro');
+    const container = document.querySelector('.container');
+    container.innerHTML = `
+    <h2>Game OVER!</h2><h3>Your total points: ${pointsMemory}</h3>`;
   }
 };
 
